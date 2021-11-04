05fd9821ff45c916ae4672939fd29527a8324378
xiangwenkai <93317912+xiangwenkai@users.noreply.github.com>
2021-11-04 16:53:10 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
 import numpy as np
 import networkx as nx
 import scipy.sparse as sp
-from ppr import calc_ppr_exact
-from ppr import calc_ppr
+from ppr import *
+import dynamic_ppr
 
 if __name__ == '__main__':
     alpha = 0.1
     G0 = nx.Graph()
     G0.add_edges_from([(1, 2), (2, 3), (3, 4), (3, 5), (1, 5), (2, 5), (1, 6)])
     nodes = list(G0.nodes)
-    delta_G0 = [[4, 6, 'i'], [1, 4, 'i']]
+    delta_G0 = [[4, 6, 'i'], [1, 4, 'i'], [3, 6, 'i']]
     delta_G1 = [[4, 6, 'i'], [1, 4, 'i'], [2, 6, 'i'], [3, 7, 'i'], [1, 8, 'i'], [5, 6, 'i'], [6, 7, 'i']]
-    A = nx.adjacency_matrix(G0).tocsr().astype(np.float32)
-    ppr = calc_ppr_exact(A, alpha=alpha)
-    print(np.around(ppr, 6))
-    print(np.sum(ppr, axis=1))
+    # A = nx.adjacency_matrix(G0).tocsr().astype(np.float32)
+    # ppr = calc_ppr_exact(A, alpha=alpha)
+    # print(np.around(ppr, 2))
 
-    ps, rs = calc_ppr(G0, alpha=0.1, epsilon=1e-8, nodes=nodes)
-    js = [list(ps[i].keys()) for i in ps.keys()]
+    ps, rs = calc_ppr(G0, alpha=0.1, epsilon=1e-8, nodes=list(G0.nodes))
     vals = [[ps[i][j] for j in sorted(ps[i])] for i in ps.keys()]
-    print(np.around(vals, 6))
-    print(np.sum(vals, axis=1))
-    print(np.linalg.norm(ppr-np.asarray(vals),axis=1))
-    exit()
+    print(np.around(vals, 3))
+    # print(np.linalg.norm(ppr - np.asarray(vals), axis=1))
 
-    # a = construct_sparse(js, vals, (len(nodes)+1, len(nodes)+1))
-    ps_new, rs_new = ppr.DynamicSNE(G0, delta_G=delta_G0, epsilon=0.001, alpha=0.9, S=list(G0.nodes), p_pre=ps,
-                                    r_pre=rs)
+    ps_new, rs_new = DaynamicPPE(G0, delta_G0, S=list(G0.nodes), epsilon=1e-8, alpha=0.2, p=ps, r=rs)
     js_new = [list(ps_new[i].keys()) for i in ps_new.keys()]
-    vals_new = [list(ps_new[i].values()) for i in ps_new.keys()]
+    vals_new = [[ps_new[i][j] for j in sorted(ps_new[i])] for i in ps_new.keys()]
+    print(np.round(vals_new,3))
+    ps, rs = calc_ppr(G0, alpha=0.1, epsilon=1e-8, nodes=list(G0.nodes))
+    vals = [[ps[i][j] for j in sorted(ps[i])] for i in ps.keys()]
+    print(np.linalg.norm(vals - np.asarray(vals_new), axis=1))
+
--- ppr.py
+++ ppr.py
@@ -16,6 +16,7 @@ def _calc_ppr_node(inode, G, alpha, epsilon, p_pre=None, r_pre=None):
         r[inode] = alpha
     else:
         r = r_pre
+        r[inode] = alpha
     q = [inode]
     while len(q) > 0:
         unode = q.pop()
@@ -61,38 +62,70 @@ def construct_sparse(neighbors, weights, shape):
     return sp.coo_matrix((np.concatenate(weights), (i, j)), shape)
 
 
-def DynamicSNE(G, delta_G, epsilon, alpha, S=[1, 2, 3], p_pre=None, r_pre=None):
-    for changes in delta_G:
-        u, v, op = changes[0], changes[1], changes[2]
+def ForwardPush(p, r, s, G, epsilon,alpha,beta=0.5):
+    enque = [s]
+    while len(enque)>0:
+        snode = enque.pop()
+        res = r[snode]
+        p[snode] += alpha*res
+        r[snode] = (1-alpha)*r[snode]*beta
+        for vnode in list(G.adj[snode]):
+            r[vnode] += (1-alpha)*r[snode]*(1-beta)/G.degree(snode)
+            res_vnode = r[vnode]
+            if abs(res_vnode) > epsilon * G.degree(vnode):
+                if vnode not in enque:
+                    enque.append(vnode)
+    return p, r
+
+
+def DynamicSNE(G, delta_G, epsilon, alpha, S=[1,2,3],p_pre=None, r_pre=None):
+    for change in delta_G:
+        u, v, op = change[0],change[1],change[2]
         flag = v in list(G.nodes)
+        if op == 'i':
+            G.add_edge(u, v)
+        if op == 'd':
+            G.remove_edge(u, v)
         for s in S:
             if op == 'i':
-                G.add_edge(u, v)
                 delta_p = p_pre[s][u] / (G.degree[u] - 1)
             if op == 'd':
-                G.remove_edge(u, v)
                 delta_p = -p_pre[s][u] / (G.degree[u] + 1)
             p_pre[s][u] += delta_p
             r_pre[s][u] = r_pre[s][u] - delta_p / alpha
-        if flag:
-            r_pre[u][v] = r_pre[u][v] + delta_p / alpha - delta_p
-        p_pre, r_pre = calc_ppr(G, alpha, epsilon, list(G.nodes), p_pre, r_pre)
+            if flag:
+                r_pre[s][v] = r_pre[s][v] + delta_p / alpha - delta_p
+            # p_pre[s], r_pre[s] = _calc_ppr_node(s, G, alpha=0.1, epsilon=epsilon, p_pre=p_pre[s], r_pre=r_pre[s])
+            p_pre[s], r_pre[s] = ForwardPush(p=p_pre[s], r=r_pre[s], s=s, G=G, epsilon=epsilon, alpha=alpha)
     return p_pre, r_pre
 
 
+def DaynamicPPE(G, delta_G0, S, epsilon, alpha, p, r):
+    # p = {}
+    # r = {}
+    # for s in S:
+    #     nnodes = len(G.nodes)
+    #     p[s] = dict(zip(G.nodes, [0]*nnodes))
+    #     r[s] = dict(zip(G.nodes, [0]*nnodes))
+    #     r[s][s] = 1
+    #     p[s],r[s] = ForwardPush(p[s], r[s], s, G, epsilon=epsilon, alpha=alpha)
+    p, r = DynamicSNE(G, delta_G0, epsilon, alpha, S=S, p_pre=p, r_pre=r)
+    return p, r
+
+
 def calc_A_hat(adj_matrix: sp.spmatrix) -> sp.spmatrix:
-    nnodes = adj_matrix.shape[0]
-    # A = adj_matrix + sp.eye(nnodes)
+    # nnodes = adj_matrix.shape[0]
     A = adj_matrix
     D_vec = np.sum(A, axis=1).A1
     D_vec_invsqrt_corr = 1. / D_vec
     D_invsqrt_corr = sp.diags(D_vec_invsqrt_corr)
-    return  D_invsqrt_corr @ A
+    return D_invsqrt_corr @ A
 
 
 def calc_ppr_exact(adj_matrix: sp.spmatrix, alpha: float) -> np.ndarray:
     nnodes = adj_matrix.shape[0]
-
     M = calc_A_hat(adj_matrix)
     A_inner = sp.eye(nnodes) - (1 - alpha) * M
     return alpha * np.linalg.inv(A_inner.toarray())
+
+
